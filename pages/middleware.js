cdc77404f2d2a04b5405b8b767831694ecbc9ed8
VAJRESH <vajresh005@gmail.com>
2024-01-27 15:46:29 +0530

    feat: add next cors

---
+++ pages/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import NextCors from "nextjs-cors";
+
+async function middleware(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  // Rest of the API logic
+  //   res.json({ message: "Hello NextJs Cors!" });
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
