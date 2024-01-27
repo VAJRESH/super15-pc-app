80d4dde5933a9bc328fae206bfb44751384592cd
VAJRESH <vajresh005@gmail.com>
2024-01-27 15:59:44 +0530

    fix: update middleware

---
+++ middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export default async function middleware(request) {
+  const response = NextResponse.next();
+
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization",
+  );
+
+  const origin = request.headers.get("Origin");
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Origin", origin);
+
+  return response;
+}
