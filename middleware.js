e1637368a5e1e4a74b007ec78b0f07011214d7fe
VAJRESH <vajresh005@gmail.com>
2024-01-29 17:06:18 +0530

    feat: update middleware for origin

--- middleware.js
+++ middleware.js
@@ -4,17 +4,13 @@ export default async function middleware(request) {
   const response = NextResponse.next();
 
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization",
-  );
 
   const protocol = request.headers["x-forwarded-proto"] || request.protocol;
   const host = request.headers.host;
 
   // Construct the base URL without the port number
   let baseUrl = `${protocol}://${host}`;
-  if (!protocol || !host) baseUrl = request.headers.origin;
+  if (!protocol || !host) baseUrl = request.headers.origin || "*";
 
   response.headers.set("Access-Control-Allow-Credentials", "true");
   response.headers.set("Access-Control-Allow-Origin", baseUrl);
