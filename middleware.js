e8caf665b315be10b1a1c6e2f0c9092dacf33840
VAJRESH <vajresh005@gmail.com>
2024-01-29 14:34:34 +0530

    feat: update middleware

--- middleware.js
+++ middleware.js
@@ -9,8 +9,14 @@ export default async function middleware(request) {
     "Content-Type, Authorization",
   );
 
+  const protocol = request.headers["x-forwarded-proto"] || request.protocol;
+  const host = request.headers.host;
+
+  // Construct the base URL without the port number
+  const baseUrl = `${protocol}://${host}`;
+
   response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Origin", baseUrl);
 
   return response;
 }
