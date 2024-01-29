189ace5af0033a5278914d423859bc33cff92266
VAJRESH <vajresh005@gmail.com>
2024-01-29 18:19:41 +0530

    feat: update middleware for header

--- middleware.js
+++ middleware.js
@@ -4,17 +4,9 @@ export default async function middleware(request) {
   const response = NextResponse.next();
 
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-
-  const protocol = request.headers["x-forwarded-proto"] || request.protocol;
-  const host = request.headers.host;
-
-  // Construct the base URL without the port number
-  let baseUrl = `${protocol}://${host}`;
-  if (!protocol || !host) baseUrl = request.headers.origin;
-  if (!baseUrl) baseUrl = request.headers.referer || "*";
-
   response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set("Access-Control-Allow-Origin", baseUrl);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Headers", "*");
 
   return response;
 }
