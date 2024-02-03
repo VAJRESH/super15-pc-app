e834a1aed66dadf9e3d9ca3e7f0a6e6a6dbcf422
VAJRESH <vajresh005@gmail.com>
2024-02-04 02:33:05 +0530

    feat: add response to options

--- pages/api/plans.js
+++ pages/api/plans.js
@@ -1,6 +1,8 @@
 import { razorpay } from "@/helper/backend.helper";
 
 export default async function plans(req, res) {
+  if (req.method === "OPTIONS") return res.status(200).json({});
+
   return new Promise(async (resolve) => {
     const response = await razorpay.plans.all();
 
